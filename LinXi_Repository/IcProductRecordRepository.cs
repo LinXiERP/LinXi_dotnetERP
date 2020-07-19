cc46740a6cca4b0fc0dd9c051b0ac01203e72b3a
CrazyTaolt <1184395332@qq.com>
2020-07-19 14:23:53 +0800

    林涛：完成四张表的接口实现层

---
+++ LinXi_Repository/IcProductRecordRepository.cs
@@ -0,0 +1,16 @@
+﻿using LinXi_IRepository;
+using LinXi_Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinXi_Repository
+{
+    public class IcProductRecordRepository : BaseRepository<IcProductRecord>, IIcProductRecordRepository
+    {
+        public IcProductRecordRepository(DbContext db) : base(db)
+        {
+        }
+    }
+}
---
+++ LinXi_Service/IcProductStockService.cs
@@ -0,0 +1,16 @@
+﻿using LinXi_IRepository;
+using LinXi_IService;
+using LinXi_Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinXi_Service
+{
+    public class IcProductStockService : BaseService<IcProductStock>, IIcProductStockService
+    {
+        public IcProductStockService(IIcProductStockRepository IcProductStockRepository) : base(IcProductStockRepository)
+        {
+        }
+    }
+}
