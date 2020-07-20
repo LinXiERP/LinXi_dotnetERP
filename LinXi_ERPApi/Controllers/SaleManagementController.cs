0770a63f1e752e08f4fae67ccd77c6bcfdbfa913
LB <1137751803@qq.com>
2020-07-20 23:44:08 +0800

    廖彬：新增销售管理模块

---
+++ LinXi_ERPApi/Controllers/SaleManagementController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LinXi_ERPApi.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    [Authorize]
+    public class SaleManagementController : ControllerBase
+    {
+
+    }
+}
