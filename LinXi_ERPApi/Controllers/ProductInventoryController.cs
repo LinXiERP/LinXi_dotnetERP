08c42dc3abf18df7627f87c042acb5fa20c5e84d
邓伴林 <2593210042@qq.com>
2020-07-21 00:29:34 +0800

    邓伴林：新建了产品库存管理控制器

---
+++ LinXi_ERPApi/Controllers/ProductInventoryController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LinXi_ERPApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductInventoryController : ControllerBase
+    {
+
+    }
+}
