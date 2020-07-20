bb04ad790d6a0e01639c7ac77e8b7df28a1d9202
CrazyTaolt <1184395332@qq.com>
2020-07-20 20:27:12 +0800

    林涛：新建生产管理模块控制器

---
+++ LinXi_ERPApi/Controllers/ProductionManagementController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using LinXi_IService;
+using LinXi_Model;
+
+//using LinXi_Model.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace LinXi_ERPApi.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    [Authorize]
+    public class ProductionManagementController : ControllerBase
+    {
+    }
+}
