218f1ee0ecab0749bd9f16d36ffa9976a77f8246
敢敢 <chenjunhao@163.com>
2020-07-21 00:48:41 +0800

    陈俊豪：创建商品库存管理控制器

---
+++ LinXi_ERPApi/Controllers/CommodityInventoryController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using LinXi_IService;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace LinXi_ERPApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommodityInventoryController : ControllerBase
+    {
+        private ILogger<CommodityInventoryController> _logger;
+
+        private IAcDepartmentService _IAcDepartmentService;
+        private IServiceProvider _service;
+
+        private IMapper _IMapper;
+        private IHttpContextAccessor _httpContext;
+    }
+}
