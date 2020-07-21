04781fc7dc5107ad7d7c08d3e6f2b77e18ada60f
邓伴林 <2593210042@qq.com>
2020-07-21 17:10:43 +0800

    邓伴林：四张表的接口实现

---
+++ LinXi_Repository/PrProductCategoryRepository.cs
@@ -0,0 +1,16 @@
+﻿using LinXi_IRepository;
+using LinXi_Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinXi_Repository
+{
+    public class PrProductCategoryRepository : BaseRepository<PrProductCategory>, IPrProductCategoryRepository
+    {
+        public PrProductCategoryRepository(DbContext db) : base(db)
+        {
+        }
+    }
+}
---
+++ LinXi_Service/PrProductCategoryService.cs
@@ -0,0 +1,16 @@
+﻿using LinXi_IRepository;
+using LinXi_IService;
+using LinXi_Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinXi_Service
+{
+    public class PrProductCategoryService : BaseService<PrProductCategory>, IPrProductCategoryService
+    {
+        public PrProductCategoryService(IPrProductCategoryRepository IPrProductCategoryRepository) : base(IPrProductCategoryRepository)
+        {
+        }
+    }
+}
