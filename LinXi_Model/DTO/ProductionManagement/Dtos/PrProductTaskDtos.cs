abed4671f2eb7497129dd5d315359252f412e37d
CrazyTaolt <1184395332@qq.com>
2020-07-22 22:51:00 +0800

    林涛：新增生产管理生产计划表dto类

---
+++ LinXi_Model/DTO/ProductionManagement/Dtos/PrProductTaskDtos.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinXi_Model.DTO.ProductionManagement.Dtos
+{
+    public class PrProductTaskDtos
+    {
+        public int Id { get; set; }
+        public string No { get; set; }
+        public int? ProductId { get; set; }
+        public decimal? Nums { get; set; }
+        public DateTime? ProductDate { get; set; }
+        public string Batch { get; set; }
+        public int? DepartmentId { get; set; }
+        public int? OperatorId { get; set; }
+        public DateTime? OperateTime { get; set; }
+        public int? Status { get; set; }
+        public string Remark { get; set; }
+        public int? QmId { get; set; }
+        public string ProductName { get; set; }
+        public int? DepartmentName { get; set; }
+        public int? OperatorName { get; set; }
+    }
+}
---
+++ LinXi_Model/DTO/ProductionManagement/Profiles/PrProductTaskProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using LinXi_Model.DTO.ProductionManagement.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinXi_Model.DTO.ProductionManagement.Profiles
+{
+    public class PrProductTaskProfile : Profile
+    {
+        public PrProductTaskProfile()
+        {
+            CreateMap<PrProductTask, PrProductTaskDtos>();
+            CreateMap<PrProductTaskDtos,PrProductTask>();
+        }
+    }
+}
