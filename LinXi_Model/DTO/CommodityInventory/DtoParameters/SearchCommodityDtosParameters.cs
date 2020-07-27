bd0b8187592eefb7d754504331236d174997267b
敢敢 <chenjunhao@163.com>
2020-07-27 23:34:20 +0800

    陈俊豪：同步

---
+++ LinXi_Model/DTO/CommodityInventory/DtoParameters/SearchCommodityDtosParameters.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinXi_Model.DTO.CommodityInventory.DtoParameters
+{
+  public  class SearchCommodityDtosParameters
+    {
+        public string name { get; set; }
+        public int category { get; set; }
+    }
+}
