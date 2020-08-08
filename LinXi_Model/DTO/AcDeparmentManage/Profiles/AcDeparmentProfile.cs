7e61fb7e5da1d55cff30f36f8a10e9a7dff61da9
YaoJinJian <499283568@qq.com>
2020-08-08 11:55:45 +0800

    员工管理同步

---
+++ LinXi_Model/DTO/AcDeparmentManage/Profiles/AcDeparmentProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using LinXi_Model.DTO.AcDeparmentManage.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinXi_Model.DTO.AcDeparmentManage.Profiles
+{
+    public class AcDepartmentProfile : Profile
+    {
+        public AcDepartmentProfile()
+        {
+            CreateMap<AcDepartment, AcDeparmentDtos>();
+            CreateMap<AcDeparmentDtos, AcDepartment>();
+        }
+    }
+}
---
+++ LinXi_Model/DTO/StaffManagement/DtoParameters/AcStaffDtosParameters.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinXi_Model.DTO.StaffManagement.DtoParameters
+{
+   public class AcStaffDtosParameters
+    {
+        public string name { get; set; }
+        public int deparmentId { get; set; }
+        public int select { get; set; }
+    }
+}
