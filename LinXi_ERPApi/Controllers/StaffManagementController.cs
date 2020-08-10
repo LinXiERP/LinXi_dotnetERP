900d89d1fc3527997d38b25ed3b7b31ab3d9f546
YaoJinJian <499283568@qq.com>
2020-08-10 23:49:54 +0800

    姚金建：回退

--- LinXi_ERPApi/Controllers/StaffManagementController.cs
+++ LinXi_ERPApi/Controllers/StaffManagementController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using AutoMapper;
 using LinXi_IService;
 using LinXi_Model;
+using LinXi_Model.DTO.StaffManagement.DtoParameters;
 using LinXi_Model.DTO.StaffManagement.Dtos;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
@@ -35,10 +36,31 @@ namespace LinXi_ERPApi.Controllers
         /// 获取所有的员工信息
         /// </summary>
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<AcStaffDtos>>> GetAllStaffInfo()
+        public async Task<ActionResult<IEnumerable<AcStaff>>> GetAllStaffInfo()
         {
-            var data = _mapper.Map<IEnumerable<AcStaffDtos>>(await _IAcStaffService.Search(t => true));
-            return Ok(data);
+            var data = (await _IAcStaffService.Search(t => true)).ToList();
+            var data2 = _mapper.Map<List<AcStaffDtos>>(data);
+            foreach (var item in data2)
+            {
+                if (item.Sex==1)
+                {
+                    item.SexName = "男";
+                    
+                }
+                else
+                {
+                    item.SexName = "女";
+                }
+                if (item.Status==1)
+                {
+                    item.StatusStr = "在职";
+                }
+                else
+                {
+                    item.StatusStr = "离职";
+                }
+            }
+            return Ok(data2);
         }
         /// <summary>
         /// 获取指定员工的详细信息
@@ -55,7 +77,7 @@ namespace LinXi_ERPApi.Controllers
         /// </summary>
         /// <param name="staff"></param>
         /// <returns></returns>
-        [HttpGet]
+        [HttpPost]
         public async Task<int> AddStaff(AcStaff staff)
         {
             return await _IAcStaffService.Add(staff); ;
@@ -63,12 +85,17 @@ namespace LinXi_ERPApi.Controllers
         /// <summary>
         /// 修改员工信息
         /// </summary>
-        /// <param name="staff"></param>
-        /// <returns></returns>
         [HttpPut]
-        public async Task<int> EditStaff(AcStaff staff)
+        public async Task<ActionResult<InfoResult<AcStaffDtos>>> UpdateStaff(AcStaffDtos acStaffDtos)
         {
-            return await _IAcStaffService.Edit(staff); ;
+            var data = await _IAcStaffService.Edit(_mapper.Map<AcStaff>(acStaffDtos));
+            InfoResult<AcStaffDtos> messageModel = new InfoResult<AcStaffDtos>();
+            if (data > 0) { messageModel.Msg = "更新成功"; messageModel.Code = 400; messageModel.Success = true; }
+            else
+            {
+                messageModel.Msg = "更新失败"; messageModel.Code = 201; messageModel.Success = false;
+            }
+            return Ok(messageModel);
         }
         /// <summary>
         /// 删除客户信息
@@ -90,6 +117,34 @@ namespace LinXi_ERPApi.Controllers
         public async Task<int> DeleteStaff(AcStaff staff)
         {
             return await _IAcStaffService.Delete(staff); ;
+        }
+        /// <summary>
+        /// 过滤员工信息
+        /// </summary>
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AcStaffDtos>>> SelectAcStaffInfo([FromQuery] AcStaffDtosParameters staffDtosParameters)
+        {
+            IEnumerable<AcStaff> list = _mapper.Map<IEnumerable<AcStaff>>(await _IAcStaffService.Search(t => true));
+            if (!string.IsNullOrWhiteSpace(staffDtosParameters.name))
+            {   
+                switch (staffDtosParameters.select)
+                {
+                    case 1:
+                        {
+                            var data = list.Where(t => t.Name.Contains(staffDtosParameters.name)).ToList();
+                            return Ok(data);
+                        }
+
+                    case 2:
+                        {
+                            var data = list.Where(t => t.DepartmentId==staffDtosParameters.deparmentId).ToList();
+                            return Ok(data);
+                        }
+                   
+                }
+            }
+            return Ok(list);
+
         }
     }
 }
